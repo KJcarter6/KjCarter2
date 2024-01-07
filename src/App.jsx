81faf1ff15e00f11d9a6e866fb8a125c02b5144e
Kj Carter <kobecarter00@gmail.com>
2024-01-06 19:30:52 -0700

    kjcommit1

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-        <color attach="background" args={["#171720"]} />
-        <fog attach="fog" args={["#171720", 10, 30]} />
+        <color attach="background" args={["#121213"]} />
+        <fog attach="fog" args={["#111113", 10, 30]} />
         <Suspense>
           <Experience />
         </Suspense>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,7 @@ import { degToRad, lerp } from "three/src/math/MathUtils";
 import { Camping } from "./Camping";
 import { currentPageAtom } from "./UI";
 
-const bloomColor = new Color("#fff");
+const bloomColor = new Color("#7b4ad4");
 bloomColor.multiplyScalar(1.5);
 
 export const Experience = () => {
@@ -44,7 +44,7 @@ export const Experience = () => {
 
   const fitCamera = async () => {
     if (currentPage === "store") {
-      controls.current.smoothTime = 0.8;
+      controls.current.smoothTime = 1.5;
       controls.current.fitToBox(meshFitCameraStore.current, true);
     } else {
       controls.current.smoothTime = 1.6;
@@ -66,43 +66,234 @@ export const Experience = () => {
     <>
       <CameraControls ref={controls} />
       <mesh ref={meshFitCameraHome} position-z={1.5} visible={false}>
-        <boxGeometry args={[7.5, 2, 2]} />
+        <boxGeometry args={[7.4, 2.1, 2]} />
         <meshBasicMaterial color="orange" transparent opacity={0.5} />
       </mesh>
+
       <Text
-        font={"fonts/Poppins-Black.ttf"}
-        position-x={-1.3}
+        font={"fonts/RubikGlitch-Regular.ttf"}
+        position-x={-.6}
         position-y={-0.5}
         position-z={1}
         lineHeight={0.8}
         textAlign="center"
-        rotation-y={degToRad(30)}
+        rotation-y={degToRad(28)}
         anchorY={"bottom"}
       >
-        MY LITTLE{"\n"}CAMPING
+        KJ CARTER'S{"\n"}PORTFOLIO
         <meshBasicMaterial
           color={bloomColor}
           toneMapped={false}
           ref={textMaterial}
         >
           <RenderTexture attach={"map"}>
-            <color attach="background" args={["#fff"]} />
+            <color attach="background" args={["#c9c7c7"]} />
             <Environment preset="sunset" />
-            <Float floatIntensity={4} rotationIntensity={5}>
+            <Float floatIntensity={4} rotationIntensity={4}>
               <Camping
-                scale={1.6}
-                rotation-y={-degToRad(25)}
-                rotation-x={degToRad(40)}
+                scale={10}
+                rotation-y={-degToRad(180)}
+                rotation-x={degToRad(50)}
                 position-y={-0.5}
               />
             </Float>
           </RenderTexture>
         </meshBasicMaterial>
       </Text>
-      <group rotation-y={degToRad(-25)} position-x={3}>
-        <Camping scale={0.6} html />
-        <mesh ref={meshFitCameraStore} visible={false}>
-          <boxGeometry args={[2, 1, 2]} />
+      <Text
+        font={"fonts/Tektur-VariableFont_wdth,wght.ttf"}
+        scale={.19}
+        position-x={.8}
+        position-y={1.5}
+        position-z={.08}
+        lineHeight={-0.4}
+        textAlign="center"
+        rotation-y={degToRad(1)}
+        anchorY={"bottom"}
+      >
+        
+        
+
+        EXPERIENCE
+        
+        
+        <meshBasicMaterial
+          color={bloomColor}
+          toneMapped={false}
+         
+        >
+          <RenderTexture attach={"map"}>
+            <color attach="background" args={["#c9c7c7"]} />
+            <Environment preset="sunset" />
+            <Float floatIntensity={4} rotationIntensity={4}>
+              <Camping
+                scale={0}
+                rotation-y={-degToRad(180)}
+                rotation-x={degToRad(50)}
+                position-y={-0.5}
+              />
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
+        
+      </Text>
+      <Text
+        font={"fonts/Tektur-VariableFont_wdth,wght.ttf"}
+        scale={.07}
+        position-x={.8}
+        position-y={1.2}
+        position-z={.10}
+        lineHeight={-0.4}
+        textAlign="center"
+        rotation-y={degToRad(1)}
+        anchorY={"bottom"}
+      >
+  Using SEO principles and maintable code to deliver the best experience for users and my employer  {"\n"} {"\n"}
+                                          website for a growing publishing company based out of Utah{"\n"} {"\n"} 
+      Created an engaging and responsive ecommerce{"\n"}{"\n"}
+      
+      {"\n"}
+      
+        
+        
+        <meshBasicMaterial
+          color={bloomColor}
+          toneMapped={false}
+         
+        >
+          <RenderTexture attach={"map"}>
+            <color attach="background" args={["#c9c7c7"]} />
+            <Environment preset="sunset" />
+            <Float floatIntensity={4} rotationIntensity={4}>
+              <Camping
+                scale={0}
+                rotation-y={-degToRad(180)}
+                rotation-x={degToRad(50)}
+                position-y={-0.5}
+              />
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
+        
+      </Text>
+      <Text
+        font={"fonts/Tektur-VariableFont_wdth,wght.ttf"}
+        scale={.09}
+        position-x={.8}
+        position-y={1.3}
+        position-z={.10}
+        lineHeight={-0.4}
+        textAlign="center"
+        rotation-y={degToRad(1)}
+        anchorY={"bottom"}
+      >
+      Bjork Print as Front-End Developer | Sep 22 - current{"\n"}{"\n"}
+      {"\n"}
+      
+        
+        
+        <meshBasicMaterial
+          color={bloomColor}
+          toneMapped={false}
+         
+        >
+          <RenderTexture attach={"map"}>
+            <color attach="background" args={["#c9c7c7"]} />
+            <Environment preset="sunset" />
+            <Float floatIntensity={4} rotationIntensity={4}>
+              <Camping
+                scale={0}
+                rotation-y={-degToRad(180)}
+                rotation-x={degToRad(50)}
+                position-y={-0.5}
+              />
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
+        
+      </Text>
+
+      <Text
+        font={"fonts/Tektur-VariableFont_wdth,wght.ttf"}
+        scale={.1}
+        position-x={.8}
+        position-y={.7}
+        position-z={.10}
+        lineHeight={-0.4}
+        textAlign="center"
+        rotation-y={degToRad(1)}
+        anchorY={"bottom"}
+      >
+        
+
+      Freelance as Full-Stack Developer | Three years {"\n"}
+     
+        
+        
+        <meshBasicMaterial
+          color={bloomColor}
+          toneMapped={false}
+         
+        >
+          <RenderTexture attach={"map"}>
+            <color attach="background" args={["#c9c7c7"]} />
+            <Environment preset="sunset" />
+            <Float floatIntensity={4} rotationIntensity={4}>
+              <Camping
+                scale={0}
+                rotation-y={-degToRad(180)}
+                rotation-x={degToRad(50)}
+                position-y={-0.5}
+              />
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
+        
+      </Text>
+
+      <Text
+        font={"fonts/Tektur-VariableFont_wdth,wght.ttf"}
+        scale={.1}
+        position-x={.8}
+        position-y={.6}
+        position-z={.10}
+        lineHeight={-0.4}
+        textAlign="center"
+        rotation-y={degToRad(1)}
+        anchorY={"bottom"}
+      >
+        
+        {"\n"}{"\n"}
+
+      YHA as Shift Lead | Two Years
+      {"\n"}
+      {"\n"}
+        
+        <meshBasicMaterial
+          color={bloomColor}
+          toneMapped={false}
+         
+        >
+          <RenderTexture attach={"map"}>
+            <color attach="background" args={["#c9c7c7"]} />
+            <Environment preset="sunset" />
+            <Float floatIntensity={4} rotationIntensity={4}>
+              <Camping
+                scale={0}
+                rotation-y={-degToRad(180)}
+                rotation-x={degToRad(50)}
+                position-y={-0.5}
+              />
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
+        
+      </Text>
+      
+      <group rotation-y={degToRad(160)} position-x={2.65} position-y={-.35}>
+        <Camping scale={4} html />
+        <mesh ref={meshFitCameraStore} visible={true}>
+          <boxGeometry args={[-1, -4.5, -.12]} />
           <meshBasicMaterial color="red" transparent opacity={0.5} />
         </mesh>
       </group>
@@ -119,7 +310,7 @@ export const Experience = () => {
           transparent
           minDepthThreshold={0.4}
           maxDepthThreshold={1.4}
-          color="#333"
+          color="#1f1e1e"
           metalness={0.5}
         />
       </mesh>
@@ -128,4 +319,4 @@ export const Experience = () => {
   );
 };
 
-useFont.preload("fonts/Poppins-Black.ttf");
+useFont.preload("fonts/RubikGlitch-Regular.ttf");
